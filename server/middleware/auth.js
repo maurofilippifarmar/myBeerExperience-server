1eb7cf3673871d1c6be94142a66a0b5ac55574b7
Mohammed Al-shameri <malshameri54@gmail.com>
2023-04-18 12:00:10 +0200

    updsating the middlewares validators

---
+++ server/middleware/auth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import UserCollection from '../models/userModels.js';
+
+export const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.token;
+        // verify this token
+        const payload = jwt.verify(token, process.env.SIGNATURE); // return the payload
+
+        // find the user by id
+        const user = await UserCollection.findById(payload._id);
+
+        // attaching user in request object
+        req.user = user;
+        next()
+
+    } catch (error) {
+        res.json({success:false, message:error.message})
+    }
+}
+
--- server/models/userModels.js
+++ server/models/userModels.js
@@ -6,6 +6,10 @@ const userSchema = new Schema({
         required: true,
         unique: true,
     },
+    age: {
+        type: Number,
+        required: true,
+    },
     email: {
         type: String,
         required: true,
